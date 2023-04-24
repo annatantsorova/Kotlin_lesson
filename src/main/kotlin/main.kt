d6fa5df7c97805411abd5748053af32ead75dbb4
Tantsorova Anna <annukk17@gmail.com>
2023-04-24 10:07:31 +0300

    Kotlin_Lesson-main,println,print,comments

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Greetings of the day!")
+    println("My name is Anna and i live in Russia.")
+    //println("I have technical education")
+
+    print("I successfully finished Web-development course,")
+    print(" html, css, bootstrap, git")
+    /*
+    * print(" Now on i am concentrated on android development")
+    * print(" and happy to be a part of such a ")
+    * print(" wonderful and deep IT world")*/
+}
+
+
